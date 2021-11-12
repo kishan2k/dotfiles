7699ca12173f5a67ec44419dc26fd8a43867a4b2
Kishan <kishan+git@ambasana.me>
2021-11-12 00:30:23 +0000

    update

--- dot_dots/custom-functions.zsh
+++ dot_dots/custom-functions.zsh
@@ -2,22 +2,22 @@
 
 # switch to cr-dev
 function cr-dev() {
-	_evalcache assume-role cr-dev
+	eval $( $(which assume-role) cr-dev);
 }
 
 # switch to cr-staging
 function cr-staging() {
-	_evalcache assume-role cr-staging
+	eval $( $(which assume-role) cr-staging);
 }
 
 # switch to cr-prod
 function cr-prod() {
-	_evalcache assume-role cr-prod
+	eval $( $(which assume-role) cr-prod);
 }
 
 function vpy() {
-	_evalcache pyenv init -
-	_evalcache pyenv virtualenv-init -
+	eval $( pyenv init - )
+	eval $( pyenv virtualenv-init - )
 	pyenv virtualenv $2 pyenv-$1 && source activate pyenv-$1 || source activate pyenv-$1
 }
 
